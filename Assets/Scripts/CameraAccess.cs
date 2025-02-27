60b6c351b310bf268a2e8b151626163d73a59a35
nameslessjin <42518630+nameslessjin@users.noreply.github.com>
2025-02-26 17:22:46 -0800

    Initial commit

---
+++ Assets/Scripts/CameraAccess.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CameraAccess : MonoBehaviour
+{
+    public RawImage CameraRawImage;
+
+    private ARCameraManager _cameraManager;
+    private Texture2D _cameraTexture;
+    private XRCpuImage _lastCpuImage;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _cameraManager.frameReceived += OnFrameReceived;
+    }
+
+    private void OnFrameReceived(ARCameraFrameEventArgs args)
+    {
+        _lastCpuImage = new XRCpuImage();
+        if (!_cameraManager.TryAcquireLatestCpuImage(out _lastCpuImage))
+        {
+            return;
+        }
+
+        UpdateCameraTexture(_lastCpuImage);
+    }
+
+    private unsafe void UpdateCameraTexture(XRCpuImage image)
+    {
+        var format = TextureFormat.RGBA32;
+
+        if (_cameraTexture == null || _cameraTexture.width != image.width || _cameraTexture.height != image.height)
+        {
+            _cameraTexture = new Texture2D(image.width, image.height, format, false);
+        }
+
+        var conversionParams = new XRCpuImage.ConversionParams(image, format);
+        var rawTextureData = _cameraTexture.GetRawTextureData<byte>();
+
+        try
+        {
+            image.Convert(conversionParams, new IntPtr(rawTextureData.GetUnsafePtr()), rawTextureData.Length);
+        }
+        finally
+        {
+            image.Dispose();
+        }
+
+        _cameraTexture.Apply();
+        CameraRawImage.texture = _cameraTexture;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
